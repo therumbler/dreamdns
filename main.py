7f27d694bb19305b9c8978f9b2f5941cf69f9e70
Benjamin Rumble <therumbler@gmail.com>
2020-07-22 23:54:32 -0400

    get current ip and update dns record if missing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from urllib.request import urlopen
 import sys
 
 from dreamhost import _call
@@ -19,6 +20,19 @@ def _get_dns_ip(dns_records):
 
     logger.error('cannot find %s record for %s', rec_type, DREAMHOST_DNS_DOMAIN)
 
+def get_current_ip():
+    """fetch the current public IP address"""
+    url = "https://api.ipify.org/"
+    public_ip = urlopen(url).read().decode()
+    logger.debug('public_id = %s', public_ip)
+    return public_ip
+
+def dns_add_record(current_ip):
+    cmd = 'dns-add_record'
+    logger.info('creating record for %s', current_ip)
+    resp = _call(cmd, record=DREAMHOST_DNS_DOMAIN, type='A', value=current_ip)
+    logger.info('attempted to create dns %r', resp)
+    
 
 def dynamic_dns():
     """fix dns record"""
@@ -26,6 +40,13 @@ def dynamic_dns():
     dns_records = _call(cmd)
     dns_ip = _get_dns_ip(dns_records)
     logger.info('dns_ip = %s', dns_ip)
+    current_ip = get_current_ip()
+    if dns_ip == current_ip:
+        logger.info('dns_ip and current_ip are both %s', dns_ip)
+        return
+    logger.error('ip mismatch. dns: %s. current %s', dns_ip, current_ip)
+    if not dns_ip:
+        dns_add_record(current_ip)
 
 
 def main():
